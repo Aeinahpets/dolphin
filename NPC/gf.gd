61f24565c9434aad684af84c10031051b4b387de
Aeinahpets <38909120+Aeinahpets@users.noreply.github.com>
2025-08-13 21:18:09 +0200

    dialogue

--- NPC/gf.gd
+++ NPC/gf.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+
+@onready var label: Label = $Label
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action("interact") and label.visible:
+		play_dialogue()
+
+
+func play_dialogue():
+	DialogueManager.show_dialogue_balloon(load("res://Diologues/talk_to_GF.dialogue"), "first_interaction")
+
+func _on_interaction_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		label.visible = true
+
+
+func _on_interaction_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		label.visible = false
